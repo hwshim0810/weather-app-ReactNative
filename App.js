b7db96d25c22806ff95761db2b10d4e724d6a724
hyunwoo.shim <shim.hw0810@gmail.com>
2018-02-04 16:11:03 +0900

    Project init

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+});
