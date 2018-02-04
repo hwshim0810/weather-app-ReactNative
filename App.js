29cfe799ed3a6c2b5d8c6400d1ba9f409fef22c7
hyunwoo.shim <shim.hw0810@gmail.com>
2018-02-04 16:32:53 +0900

    Loading screen upload

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  }
   render() {
+    const { isLoaded } = this.state;  // equals: const isLoaded = this.state.isLoaded;
     return (
       <View style={styles.container}>
+        {isLoaded ? null : (
+            <View style={styles.loading}>
+              <Text style={styles.loadingText}>Now Loading...</Text>
+            </View>
+          )}
       </View>
     );
   }
@@ -13,8 +22,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    backgroundColor: '#fff'
+  },
+  loading: {
+    flex:1,
+    backgroundColor: "#b3d7fc",
+    justifyContent: "flex-end",
+    paddingLeft: 44
+  },
+  loadingText: {
+    fontSize: 33,
+    marginBottom: 120
   }
 });
